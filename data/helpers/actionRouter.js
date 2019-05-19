85bf95161cd06944fe7a8aec949c30d2d619ce06
JustinAndrade <j.andrade.john@gmail.com>
2019-05-18 22:10:48 -0400

    Project CRUD Completed

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -6,8 +6,19 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
 	try {
-		const actions = await Actions.get(req.query);
+		const actions = await Actions.get(req.id);
 		res.status(200).json(actions);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ message: 'Error retrieving actions.' });
+	}
+});
+
+router.get('/:id', async (req, res) => {
+	try {
+		const { id } = req.params;
+		const action = await Actions.get(id);
+		res.status(200).json(action);
 	} catch (err) {
 		console.log(err);
 		res.status(500).json({ message: 'Error retrieving actions.' });
